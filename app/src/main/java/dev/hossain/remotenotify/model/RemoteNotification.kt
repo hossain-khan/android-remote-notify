c208d068ca63a09de46f500864f15b0d295343c8
Hossain Khan <hello@hossain.dev>
2025-02-09 18:30:29 -0500

    [FIXED] Delete feature and updating list

--- app/src/main/java/dev/hossain/remotenotify/model/RemoteNotification.kt
+++ app/src/main/java/dev/hossain/remotenotify/model/RemoteNotification.kt
@@ -7,11 +7,15 @@ const val BATTERY_PERCENTAGE_NONE = -1
 const val STORAGE_MIN_SPACE_GB_NONE = -1
 
 sealed interface RemoteNotification {
+    val alertId: Long
+
     data class BatteryNotification(
+        override val alertId: Long = 0,
         val batteryPercentage: Int,
     ) : RemoteNotification
 
     data class StorageNotification(
+        override val alertId: Long = 0,
         val storageMinSpaceGb: Int,
     ) : RemoteNotification
 }
@@ -20,20 +24,22 @@ fun RemoteNotification.toNotificationEntity(): NotificationEntity =
     when (this) {
         is RemoteNotification.BatteryNotification ->
             NotificationEntity(
-                type = NotificationType.BATTERY,
+                id = alertId,
                 batteryPercentage = batteryPercentage,
+                type = NotificationType.BATTERY,
                 storageMinSpaceGb = 0,
             )
         is RemoteNotification.StorageNotification ->
             NotificationEntity(
-                type = NotificationType.STORAGE,
+                id = alertId,
                 storageMinSpaceGb = storageMinSpaceGb,
+                type = NotificationType.STORAGE,
                 batteryPercentage = 0,
             )
     }
 
 fun NotificationEntity.toRemoteNotification(): RemoteNotification =
     when (type) {
-        NotificationType.BATTERY -> RemoteNotification.BatteryNotification(batteryPercentage)
-        NotificationType.STORAGE -> RemoteNotification.StorageNotification(storageMinSpaceGb)
+        NotificationType.BATTERY -> RemoteNotification.BatteryNotification(id, batteryPercentage)
+        NotificationType.STORAGE -> RemoteNotification.StorageNotification(id, storageMinSpaceGb)
     }
