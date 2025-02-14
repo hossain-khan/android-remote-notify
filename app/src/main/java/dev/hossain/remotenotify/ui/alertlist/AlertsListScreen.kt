8cf6ab7f2bb5058eed1efb854095011a73a6cedf
Hossain Khan <hello@hossain.dev>
2025-02-14 11:44:23 -0500

    [UI UPDATE] Refactored list item to be more material like

--- app/src/main/java/dev/hossain/remotenotify/ui/alertlist/AlertsListScreen.kt
+++ app/src/main/java/dev/hossain/remotenotify/ui/alertlist/AlertsListScreen.kt
@@ -10,14 +10,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -27,6 +32,7 @@ import androidx.compose.runtime.produceState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
@@ -225,6 +231,7 @@ private fun NoNotifierConfiguredCard(
             modifier
                 .fillMaxWidth()
                 .padding(8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
@@ -259,26 +266,39 @@ fun NotificationItem(
                 .padding(8.dp)
                 .fillMaxWidth(),
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            when (notification) {
-                is RemoteNotification.BatteryNotification -> {
-                    Text(text = "Battery Alert")
-                    Text(text = "Battery Percentage: ${notification.batteryPercentage}%")
+        ListItem(
+            colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+            headlineContent = {
+                when (notification) {
+                    is RemoteNotification.BatteryNotification -> {
+                        Text(text = "Battery Alert")
+                    }
+                    is RemoteNotification.StorageNotification -> {
+                        Text(text = "Storage Alert")
+                    }
                 }
-
-                is RemoteNotification.StorageNotification -> {
-                    Text(text = "Storage Alert")
-                    Text(text = "Minimum Storage Space: ${notification.storageMinSpaceGb} GB")
+            },
+            supportingContent = {
+                when (notification) {
+                    is RemoteNotification.BatteryNotification -> {
+                        Text(text = "Battery Percentage: ${notification.batteryPercentage}%")
+                    }
+                    is RemoteNotification.StorageNotification -> {
+                        Text(text = "Minimum Storage Space: ${notification.storageMinSpaceGb} GB")
+                    }
                 }
-            }
-            Spacer(modifier = Modifier.size(8.dp))
-            Row {
-                Spacer(modifier = Modifier.weight(1f))
-                Button(onClick = onDelete) {
-                    Text(text = "Delete")
+            },
+            trailingContent = {
+                IconButton(onClick = onDelete) {
+                    Icon(
+                        Icons.Default.Delete,
+                        contentDescription = "Delete Alert",
+                        modifier = Modifier.size(24.dp),
+                    )
                 }
-            }
-        }
+            },
+            modifier = Modifier.padding(horizontal = 4.dp),
+        )
     }
 }
 
