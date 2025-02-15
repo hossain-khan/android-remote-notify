1a276c14d2f85f92eb6a27c69e46e87d12fff414
Hossain Khan <hello@hossain.dev>
2025-02-15 17:07:28 -0500

    [UPDATE] Renamed package to group package together and make it clear
    Because you can't add medium

--- app/src/main/java/dev/hossain/remotenotify/ui/alertmediumconfig/ConfigureNotificationMediumScreen.kt
+++ app/src/main/java/dev/hossain/remotenotify/ui/alertmediumconfig/ConfigureNotificationMediumScreen.kt
@@ -1,4 +1,4 @@
-package dev.hossain.remotenotify.ui.addalertmedium
+package dev.hossain.remotenotify.ui.alertmediumconfig
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
