b4e0f381849288df125ef75830055044b5b8e22d
Hossain Khan <hello@hossain.dev>
2025-02-15 18:32:18 -0500

    [ADDED] Google play in app review integration for feedback and request for new medium
    Right now it's not functional because app is not published in play store.
    
    Fixes #73

---
+++ app/src/main/java/dev/hossain/remotenotify/ui/alertmediumlist/PlaystoreFeedbackSection.kt
@@ -0,0 +1,73 @@
+package dev.hossain.remotenotify.ui.alertmediumlist
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.hossain.remotenotify.theme.ComposeAppTheme
+import dev.hossain.remotenotify.utils.InAppReviewManager
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@Composable
+fun FeedbackAndRequestMediumUi() {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val activity = context as? Activity
+
+    Column(
+        modifier =
+            Modifier
+                .wrapContentSize()
+                .padding(top = 32.dp)
+                .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        OutlinedButton(
+            onClick = {
+                activity?.let {
+                    scope.launch {
+                        InAppReviewManager(it).requestReview()
+                    }
+                } ?: run {
+                    Timber.w("Activity is null, cannot request review.")
+                }
+            },
+        ) {
+            Text(
+                text = "Request New Medium",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "Provide feedback on Google Play Store and request new medium.",
+            style = MaterialTheme.typography.labelSmall,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(horizontal = 64.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRatingSection() {
+    ComposeAppTheme {
+        FeedbackAndRequestMediumUi()
+    }
+}
