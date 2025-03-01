01b4ea707e3708c9bbb90f8c4ae8cbb14abd2241
Hossain Khan <hello@hossain.dev>
2025-02-28 22:37:51 -0500

    [UPDATE] version bump for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,9 @@ android {
         applicationId = "dev.hossain.remotenotify"
         minSdk = 30
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         // 📣 Don't forget to update release notes! 🤓
-        versionName = "1.9"
+        versionName = "1.10"
 
         // Read key or other properties from local.properties
         val localProperties =
