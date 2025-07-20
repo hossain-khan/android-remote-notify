dbccac242b10d60d2b0395ac3cae990e2938df6c
Hossain Khan <hello@hossain.dev>
2025-07-20 15:24:24 -0400

    [REFACTOR] Cleanup more anvil annotation

--- app/src/main/java/dev/hossain/remotenotify/notifier/TwilioNotificationSender.kt
+++ app/src/main/java/dev/hossain/remotenotify/notifier/TwilioNotificationSender.kt
@@ -1,24 +1,24 @@
 package dev.hossain.remotenotify.notifier
 
-import com.squareup.anvil.annotations.ContributesMultibinding
 import dev.hossain.remotenotify.data.AlertFormatter
 import dev.hossain.remotenotify.data.ConfigValidationResult
 import dev.hossain.remotenotify.data.TwilioConfigDataStore
-import dev.hossain.remotenotify.di.AppScope
 import dev.hossain.remotenotify.model.AlertMediumConfig
 import dev.hossain.remotenotify.model.RemoteAlert
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoSet
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.Named
 import okhttp3.Credentials
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import timber.log.Timber
-import javax.inject.Inject
-import javax.inject.Named
 
-@ContributesMultibinding(AppScope::class)
+@ContributesIntoSet(AppScope::class)
 @Named("twilio")
+@Inject
 class TwilioNotificationSender
-    @Inject
     constructor(
         private val twilioConfigDataStore: TwilioConfigDataStore,
         private val okHttpClient: OkHttpClient,
