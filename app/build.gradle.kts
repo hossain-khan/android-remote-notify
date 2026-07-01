62e09499036497a151912dad440ae22da769e677
Hossain Khan <hello@hossain.dev>
2026-07-01 13:25:23 -0400

    Release v1.21: bump version code to 26 and update release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,9 @@ android {
         applicationId = "dev.hossain.remotenotify"
         minSdk = 30
         targetSdk = 35
-        versionCode = 25
+        versionCode = 26
         // 📣 Don't forget to update release notes! 🤓
-        versionName = "1.20"
+        versionName = "1.21"
 
         // Read key or other properties from local.properties
         val localProperties =
